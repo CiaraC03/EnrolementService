528b5eacdbc619ed051e66d043d3cb6739e0c898
G00402357@atu.ie <g00402357@atu.ie>
2024-10-21 17:02:30 +0100

    Created project

---
+++ src/main/java/ie/atu/enrolementservice/EnrolementServiceApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.enrolementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnrolementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnrolementServiceApplication.class, args);
+    }
+
+}
---
+++ src/main/java/ie/atu/enrolementservice/Students.java
@@ -0,0 +1,4 @@
+package ie.atu.enrolementservice;
+
+public class Students {
+}
